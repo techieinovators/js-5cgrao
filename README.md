2cd67a572080c8359058edf323dd0e1cd4e68159
techieinovators <techie.inovators@gmail.com>
2021-09-18 13:54:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-5cgrao
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5cgrao)
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index-with-jsx.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+}
